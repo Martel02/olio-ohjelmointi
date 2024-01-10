5640cbc689a1262e31848c28bfc09b737a8a8f2e
Cartrel <72543424+CartelSharp@users.noreply.github.com>
2024-01-10 13:54:18 +0200

    moi

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -3,46 +3,41 @@
 #include <ctime>
 
 using namespace std;
+void game(int);
 
 int main()
 {
+    int range = 0;
+    cout << "Anna suurin luku mista arvata" << endl;
+    cin >> range;
+
+    game(range);
+    return 0;
+}
+
+void game(int maxnum) {
     srand(time(NULL));
     int arvaus = 0;
-    int rng = rand() % 20;
+    int rng = rand() % maxnum;
+    int arvauksia = 0;
 
-    cout << "Arvaa luku 1-20" << endl;
+    cout << "Arvaa luku 0-"<<maxnum<< endl;
 
     while(arvaus != rng) {
         cin >> arvaus;
         if(arvaus < rng) {
             cout << "luku on suurempi" <<endl;
+            arvauksia++;
         }
         else if(arvaus > rng) {
             cout << "luku on pienempi" <<endl;
+            arvauksia++;
         }
         else if(arvaus == rng) {
+            arvauksia++;
             cout << "oikea arvaus" <<endl;
+            cout << "sinulla meni "<<arvauksia<< " arvausta" <<endl;
         }
     }
-    //void arvauspeli();
-    return 0;
 }
-/*
-void arvauspeli() {
-    int arvaus = 0;
-    int rng = rand() % 20;
 
-    cout << "Arvaa luku 1-20" << endl;
-    cin >> arvaus;
-
-    if(arvaus < rng) {
-        cout << "luku on suurempi" <<endl;
-    }
-    else if(arvaus > rng) {
-        cout << "luku on pienempi" <<endl;
-    }
-    else if(arvaus == rng) {
-        cout << "oikea arvaus" <<endl;
-    }
-}
-*/
